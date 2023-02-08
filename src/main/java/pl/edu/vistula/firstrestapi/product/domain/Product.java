28b067b0581ae22c87e814ae85ce769df0463aa0
Albert Gadomski <agadoms1@stu.vistula.edu.pl>
2023-02-08 16:40:33 +0200

    FirstRestAPI Part A

---
+++ src/main/java/pl/edu/vistula/firstrestapi/product/domain/Product.java
@@ -0,0 +1,19 @@
+package pl.edu.vistula.firstrestapi.product.domain;
+
+public class Product {
+
+    private Long id;
+    private String name;
+
+    public Product(String name) {this.name = name;}
+
+    public Long getId() {return id;}
+    public String getName() {return name;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public void setName(String name) {this.name = name;}
+
+
+
+}
---
+++ src/main/java/pl/edu/vistula/firstrestapi/product/repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package pl.edu.vistula.firstrestapi.product.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.edu.vistula.firstrestapi.product.domain.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Repository
+public class ProductRepository {
+    protected final Map<Long, Product> map = new HashMap<>();
+    protected long counter = 1;
+
+    public Product save(Product entity)
+    {
+        setId(entity);
+        return entity;
+    }
+
+    private Product setId(Product entity)
+    {
+        entity.setId(counter);
+        map.put(counter, entity);
+        counter++;
+        return entity;
+    }
+}
